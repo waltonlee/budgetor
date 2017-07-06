020a3271b19fedd74434e2bdf2a475e8bdf14a0b
Walton Lee <wlee@everquote.com>
2017-07-06 14:28:15 -0400

    login & logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
 
-  devise_for :users
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  # devise_scope :user do
+  #   post 'registrations' => 'registrations#create', :as => 'register'
+  #   post 'sessions' => 'sessions#create', :as => 'login'
+  #   delete 'sessions' => 'sessions#destroy', :as => 'logout'
+  # end
+  resources :users, only: [:show, :index]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
